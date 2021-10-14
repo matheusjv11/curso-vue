03df4613e8ccedce786a10dbebeee80e4547eb00
mfsilva <mfsilva@nelogica.com.br>
2021-10-14 16:03:43 -0300

    Continuing with monster killer project

---
+++ projeto-01-monstro/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        running: false,
+        playerLife: 100,
+        monsterLife: 100,
+    },
+    computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    }
+})
