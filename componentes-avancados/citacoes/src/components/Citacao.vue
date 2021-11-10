19ec59db5a40a4ecf15a114701a8352be0a1dceb
mfsilva <mfsilva@nelogica.com.br>
2021-11-10 18:55:25 -0300

    Finishing  up the advanced approach with components, it was great

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot name="texto"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    activated() {
+        console.log("Activate")
+    },
+    deactivated() {
+        console.log("Deactivate")
+    },
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
