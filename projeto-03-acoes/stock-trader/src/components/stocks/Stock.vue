6af61eb922cf091b4494cc9a7285854e1a77396c
mfsilva <mfsilva@nelogica.com.br>
2021-12-24 19:54:59 -0300

    I've finally started my last project in this course

---
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+      <v-card class="green darken-3 white--text">
+          <v-card-title class="headline">
+              <strong>NOME <small>(Preço: PRECO)</small></strong>
+          </v-card-title>
+      </v-card>
+      <v-card>
+          <v-container fill-height>
+              <v-text-field label="Quantidade" type="number"></v-text-field>
+              <v-btn class="green darken-3 white--text">Comprar</v-btn>
+          </v-container>
+      </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+      <Stock v-for="stock in stocks" :key="stock.id"/>
+  </v-layout>
+</template>
+
+<script>
+import Stock from './Stock.vue'
+
+export default {
+    components: { Stock },
+    data() {
+        return {
+            stocks: [
+                { id: 1, name:'BMW', price: 110 },
+                { id: 1, name:'Google', price: 200 },
+                { id: 1, name:'Apple', price: 280 },
+                { id: 1, name:'Twitter', price: 83 },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
